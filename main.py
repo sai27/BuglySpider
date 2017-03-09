bac2b837a01cd278ab6fc6f3f43f7ed422d9866d
sai27 <yubo.net@163.com>
2017-03-09 15:08:38 +0800

    fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import QQBuglySpider
 def execute_spider():
     spider_over = False
     while(spider_over == False):
-        spider = QQBuglySpider.BuglySpider('qq','pwd', 'D:/bugly_content/', 'appId', 'pid')
+        spider = QQBuglySpider.BuglySpider('qq','pwd', 'outpath', 'appId', 'pid', 'version')
         spider_over = spider.run()
         del spider
         # 如果发生异常，等待10分钟重连
